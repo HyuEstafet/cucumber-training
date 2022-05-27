0868cc29366b9e49147be22325dceb6882265435
HyuEstafet <hyulya.gyuler@estafet.com>
2022-05-27 16:22:01 +0300

    committing the second cucumber text expressions task - comparing sentences

--- src/test/java/stepDefinitions/StringExpressionsSteps.java
+++ src/test/java/stepDefinitions/StringExpressionsSteps.java
@@ -10,6 +10,8 @@ public class StringExpressionsSteps {
 
     private String inputWord;
     private boolean isCaseSensitive;
+    private String firstSentence;
+    private String secondSentence;
 
     @Given("user inputs the word {string}")
     public void user_inputs_the_word(String inputString) {
@@ -27,6 +29,34 @@ public class StringExpressionsSteps {
 
         System.out.println("Updated word: " + inputWord);
         Assert.assertTrue(StringHelper.compareWords(inputWord,finalResult,isCaseSensitive));
+    }
+
+    @Given("_First sentence is set to: {string}")
+    public void _first_sentence_is_set_to(String firstInputSentence) {
+        firstSentence = firstInputSentence;
+    }
+    @When("first occurrence of symbol {string} is removed")
+    public void first_occurrence_of_symbol_is_removed(String exclamationMark) {
+        firstSentence = firstSentence.replaceFirst(exclamationMark,"");
+        System.out.println("Updated first sentence: " + firstSentence);
+    }
+    @Given("_Second sentence is set to: {string}")
+    public void _second_sentence_is_set_to(String secondInputSentence) {
+
+        secondSentence = secondInputSentence;
+    }
+    @When("all occurrences of symbol {string} is removed")
+    public void all_occurrences_of_symbols_and_are_removed(String symbol) {
+        secondSentence = secondSentence.replace(symbol,"");
+        System.out.println("Updated second sentence: " +secondSentence);
+    }
+    @When("case sensitivity is set")
+    public void case_sensitivity_is_set() {
+
+    }
+    @Then("check the equality of _First sentence and _Second sentence")
+    public void check_the_equality_of__first_sentence_and__second_sentence() {
+        Assert.assertEquals("The sentences are not equal!",firstSentence,secondSentence);
     }
 
 }
--- src/test/resources/features/StringExpressions.feature
+++ src/test/resources/features/StringExpressions.feature
@@ -1,7 +1,7 @@
 # tests created by: Hyulya Gyuler
 Feature: Comparing words, sentences and counting words.
 
-  Rule: Remove certain characters from the input word and validate
+  Rule: Remove certain characters from the input word and then validate
     @CompareWords
     Scenario: Compare words
       Given user inputs the word "Examplesssz"
@@ -11,4 +11,15 @@ Feature: Comparing words, sentences and counting words.
 #      And the letter "t" is removed
       Then verify the result is "example"
 
-  Rule:
+  Rule: Remove punctuation symbols from the input sentence and then validate
+    @CompareSentences
+    Scenario: Compare sentences
+      Given _First sentence is set to: "My sentence!!?"
+      When first occurrence of symbol "!" is removed
+      Given _Second sentence is set to: "My sentence (and more…)"
+      When all occurrences of symbol "(" is removed
+      When all occurrences of symbol ")" is removed
+      And case sensitivity is set
+      Then check the equality of _First sentence and _Second sentence
+
+
